5e4aab0290c4e6aff8321964385e40ab5ba49eb0
sahinibrahimli <sahinibrahimle@gmail.com>
2020-04-26 16:32:44 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 $file = 'https://github.com/sahinibrahimli/drfuad/files/4535288/FUAD.pdf';
-$filename = 'SH.CV.pdf';
+$filename = 'FUAD.pdf';
 header('Content-type: application/pdf');
 header('Content-Disposition: inline; filename="' . $filename . '"');
 header('Content-Transfer-Encoding: binary');
